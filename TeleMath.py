e586b5916fc301970b7a680e6d270de3c9080477
Rusequalaizer <olympkirill@gmail.com>
2023-04-07 21:35:48 +0400

    Update TeleMath class

--- TeleMath.py
+++ TeleMath.py
@@ -146,87 +146,19 @@ class TeleMath:
             if abs(x) > 1e3:
                 exp = int(math.log10(abs(x)))
                 coefficient = x/float(10**exp)
+
                 return '{:.2f}*10^{}'.format(coefficient, exp)
             elif abs(x - round(x, 2)) < 1e-4:
                 return '{:.2f}'.format(round(x, 2))
             elif abs(x) < 1 and abs(x) > -1:
                 exp = int(math.log10(abs(x))) - 1
                 coefficient = x/float(10**exp)
+
                 return '{:.2f}*10^{}'.format(coefficient, exp)
             else:
                 return '{:.2f}'.format(x)
-            
-        def plot_approx(self, x, y):
-            fig, ax = plt.subplots()
-
-            ax.grid(True)
-            ax2 = ax.twiny()
-
-            x = [float(k) for k in x]
-            y = [float(k) for k in y]
-
-            for i in range(1, 6):
-                poly_coeffs = np.polyfit(x, y, i)
-                poly_func = np.poly1d(poly_coeffs)
-                
-                x_plot = np.linspace(x[0], x[-1], 100)
-                y_plot = poly_func(x_plot)
-                ax.plot(x_plot, y_plot, label=f'Степень: {i}')
-
-            ax.scatter(x, y)
-            ax.legend()
-
-            ax.set_xlabel('x (Для тригонометрических функций - Радианы)')
-            ax.set_ylabel('y')
-
-            ax2.set_xlim(ax.get_xlim())
-            step = (x[0] - x[-1]) / 9
-            new_ticks_location = np.arange(x[0], x[-1] + step, step)
-            ax2.set_xticks(new_ticks_location)
-            ax2.set_xticklabels(self._tick_function(new_ticks_location))
-            ax2.set_xlabel('x (Для тригонометрических функций - Градусы)')
-
-            param = len(os.listdir(path = '.'))
-            fig_name = f'plot_{param}.png'
-            plt.savefig(fig_name)
-            plt.close('all')
-
-            return fig_name
         
-        def plot_interp(self, x, y):
-            x = [float(k) for k in x]
-            y = [float(k) for k in y]
-
-            f = Rbf(x, y)
-            x_new = np.linspace(x[0], x[-1], 1000)
-            y_new = f(x_new)
-            
-            fig, ax = plt.subplots()
-
-            ax.grid(True)
-            ax2 = ax.twiny()
-            ax.plot(x_new, y_new)
-
-            ax.scatter(x, y)
-
-            ax.set_xlabel('x (Для тригонометрических функций - Радианы)')
-            ax.set_ylabel('y')
-
-            ax2.set_xlim(ax.get_xlim())
-            step = (x[0] - x[-1]) / 9
-            new_ticks_location = np.arange(x[0], x[-1] + step, step)
-            ax2.set_xticks(new_ticks_location)
-            ax2.set_xticklabels(self._tick_function(new_ticks_location))
-            ax2.set_xlabel('x (Для тригонометрических функций - Градусы)')
-
-            param = len(os.listdir(path = '.'))
-            fig_name = f'plot_{param}.png'
-            plt.savefig(fig_name)
-            plt.close('all')
-            
-            return fig_name
-        
-        def calculate_integral(self, func, a, b):
+        def calculate_integral(self, func: str, a: float, b: float) -> float:
             if a >= b:
                 return "верхнее значение не может быть меньше нижнего"
             x = sp.symbols('x')
@@ -234,7 +166,7 @@ class TeleMath:
             I = sp.integrate(f, (x, a, b))
             return float(I)
         
-        def calculate_std_dev(self, numbers):
+        def calculate_std_dev(self, numbers: list) -> float:
             n = len(numbers)
             if n < 2:
                 return 0
@@ -242,6 +174,140 @@ class TeleMath:
             variance = sum((x - mean) ** 2 for x in numbers) / (n - 1)
             return math.sqrt(variance)
         
+        def calculate_mean(self, numbers: list) -> float:
+            mean = sum(numbers) / len(numbers)
+            return mean
+
+        class Interpolation:
+            def __init__(self, x: list = None, y: list = None) -> None:
+                self._x = x
+                self._y = y
+                self._figure_name = None
+
+            def radians_to_degrees(self, x):
+                return x * 180 / np.pi
+
+            def degrees_to_radians(self, x):
+                return x * np.pi / 180
+            
+            def interpolate(self, x: list = None, y: list = None) -> None:
+                if self._x is None and self._y is None:
+                    self._x = [float(k) for k in x]
+                    self._y = [float(k) for k in y]
+                else:
+                    self._x = [float(k) for k in self._x]
+                    self._y = [float(k) for k in self._y]
+                
+                f = Rbf(self._x, self._y)
+                x_new = np.linspace(self._x[0], self._x[-1], 1000)
+                y_new = f(x_new)
+
+                fig, ax = plt.subplots()
+                ax.grid(True)
+                ax2 = ax.secondary_xaxis('top', 
+                                         functions=(self.radians_to_degrees, 
+                                                    self.degrees_to_radians)
+                                         )
+
+                ax.plot(x_new, y_new)
+                ax.scatter(self._x, self._y)
+                ax.set_xlabel('x (Для тригонометрических функций - Радианы)')
+                ax2.set_xlabel('x (Для тригонометрических функций - Градусы)')
+                ax.set_ylabel('y')
+
+                ax2.set_xlim(ax.get_xlim())
+
+                param = len(os.listdir(path = '.'))
+                self._figure_name = f'plot_{param}.png'
+                plt.savefig(self._figure_name)
+                plt.close('all')
+
+            def get_plot_name(self) -> str:
+                if self._figure_name is not None:
+                    return self._figure_name
+                
+            def set_xy(self, x: list, y: list) -> None:
+                self._x = x
+                self._y = y
+        
+        class Approximation:
+            def __init__(self, x: list = None, y: list = None) -> None:
+                self._x = x
+                self._y = y
+                self._upper_bound = None
+                self._lower_bound = None
+                self._figure_name = None
+
+            def radians_to_degrees(self, x):
+                return x * 180 / np.pi
+
+            def degrees_to_radians(self, x):
+                return x * np.pi / 180
+
+            def set_lower_bound(self, lower_bound: float) -> None:
+                self._lower_bound = lower_bound
+
+            def set_upper_bound(self, upper_bound: float):
+                self._upper_bound = upper_bound
+            
+            def set_bounds(self, lower_bound: float, upper_bound: float) -> None:
+                self._lower_bound = lower_bound
+                self._upper_bound = upper_bound
+
+            def set_xy(self, x: list, y: list) -> None:
+                self._x = x
+                self._y = y
+
+            def approximate(self, x: list = None, y: list = None, lower_bound: float = None, upper_bound: float = None) -> None:
+                if self._x is None and self._y is None:
+                    self._x = [float(k) for k in x]
+                    self._y = [float(k) for k in y]
+                else:
+                    self._x = [float(k) for k in self._x]
+                    self._y = [float(k) for k in self._y]
+
+                if self._upper_bound is None and self._lower_bound is None:
+                    self._upper_bound = upper_bound
+                    self._lower_bound = lower_bound
+
+                fig, ax = plt.subplots()
+
+                ax.grid(True)
+                ax2 = ax2 = ax.secondary_xaxis('top', 
+                                               functions=(self.radians_to_degrees, 
+                                                          self.degrees_to_radians)
+                                               )
+
+                for i in range(1, 6):
+                    poly_coeffs = np.polyfit(self._x, self._y, i)
+                    poly_func = np.poly1d(poly_coeffs)
+                    
+                    if self._upper_bound is None and self._lower_bound is None:
+                        x_plot = np.linspace(self._x[0], self._x[-1], 100)
+                        y_plot = poly_func(x_plot)
+                        ax.plot(x_plot, y_plot, label=f'Степень: {i}')
+                    else:
+                        x_plot = np.linspace(self._lower_bound, self._upper_bound, 100)
+                        y_plot = poly_func(x_plot)
+                        ax.plot(x_plot, y_plot, label=f'Степень: {i}')
+
+                ax.scatter(self._x, self._y)
+                ax.legend()
+                ax.set_xlabel('x (Для тригонометрических функций - Радианы)')
+                ax2.set_xlabel('x (Для тригонометрических функций - Градусы)')
+                ax.set_ylabel('y')
+
+                ax2.set_xlim(ax.get_xlim())
+
+                param = len(os.listdir(path = '.'))
+                self._figure_name = f'plot_{param}.png'
+                plt.savefig(self._figure_name)
+                plt.close('all')
+
+            def get_plot_name(self) -> str:
+                if self._figure_name is not None:
+                    return self._figure_name
+
 
 tmath = TeleMath()
 models = TeleMath.Models()
@@ -285,42 +351,42 @@ async def start(message):
 
 @dp.message_handler(content_types=['text'])
 async def main(message):
-    if (message.text == 'GPT'):
+    if message.text == 'GPT':
         tmath.set_mode('GPT')
-    elif (message.text == 'DALL-E'):
+    elif message.text == 'DALL-E':
         tmath.set_mode('DALL-E')
-    elif (message.text == 'Mathematica'):
+    elif message.text == 'Mathematica':
         tmath.set_mode('Mathematica')
 
-    if (message.text == 'GPT' or tmath.mode == 'GPT'):
-        if (tmath.mode == message.text):
+    if message.text == 'GPT' or tmath.mode == 'GPT':
+        if tmath.mode == message.text:
             await bot.send_message(message.chat.id, 'Пришлите любой текстовый запрос.\n\nВ ответ Вам будет выслан текст сгенерированный GPT-3.5-turbo')
 
-        if (tmath.mode == 'GPT' and message.text != 'GPT'):
+        if tmath.mode == 'GPT' and message.text != 'GPT':
             await bot.send_message(message.chat.id, models.generate_respose_gpt(message.text))
-    elif (message.text == 'DALL-E' or tmath.mode == 'DALL-E'):
-        if (tmath.mode == message.text):
+    elif message.text == 'DALL-E' or tmath.mode == 'DALL-E':
+        if tmath.mode == message.text:
             await bot.send_message(message.chat.id, 'Пришлите любой текстовый запрос.\n\nВ ответ Вам будет выслано изображение сгенерированное DALL-E')
 
-        if (tmath.mode == 'DALL-E' and message.text != 'DALL-E'):
+        if tmath.mode == 'DALL-E' and message.text != 'DALL-E':
             models.generate_respose_alpha(message.text)
             image = open('output.jpg', 'rb')
 
             await bot.send_photo(message.chat.id, image)
 
             image.close()
-    elif (message.text == 'Mathematica' or tmath.mode == 'Mathematica'):
-        if (tmath.mode == message.text):
+    elif message.text == 'Mathematica' or tmath.mode == 'Mathematica':
+        if tmath.mode == message.text:
             math_info = get_from_json('texts-for-example.json', 'math_info')
             await bot.send_message(message.chat.id, math_info)
 
-        if (tmath.mode == 'Mathematica' and message.text != 'Mathematica'):
+        if tmath.mode == 'Mathematica' and message.text != 'Mathematica':
             dict_result = {}
             parts = message.text.split(": ")
             dict_result['modificator'] = parts[0]
             dict_result['functions'] = parts[1]
 
-            if (dict_result['modificator'] == 'func'):
+            if dict_result['modificator'] == 'func':
                 fig_name = telemath.plot_math_function(dict_result['functions'])
                 derivates = telemath.derivate(dict_result['functions'])
                 integrals = telemath.find_integral(dict_result['functions'])
@@ -339,10 +405,10 @@ async def main(message):
                     msg += f'Производная функции: {derivates[i]}\n\n'
 
                     await bot.send_message(message.chat.id, msg)
-            elif (dict_result['modificator'] == 'eval'):
+            elif dict_result['modificator'] == 'eval':
                 data = dict_result['functions'].split(';')
 
-                if (len(data) == 2):
+                if len(data) == 2:
                     str_func = data[0]
                     str_values = data[1]
 
@@ -356,33 +422,53 @@ async def main(message):
                         string += f'{number}\n'
 
                     await bot.send_message(message.chat.id, string)
-            elif (dict_result['modificator'] == 'approx'):
+            elif dict_result['modificator'] == 'approx':
                 data = dict_result['functions'].split(';')
-                if (len(data) == 2):
+                if len(data) == 2:
                     x = data[0].split()
                     y = data[1].split()
-                    fig_name = telemath.plot_approx(x, y)
-                    plot_image = open(fig_name, 'rb')
+                    approx = TeleMath.Mathematica.Approximation()
+                    approx.set_xy(x, y)
+                    approx.approximate()
+                    plot_image = open(approx.get_plot_name(), 'rb')
 
                     await bot.send_photo(message.chat.id, plot_image)
 
                     plot_image.close()
-                    os.remove(fig_name)
-            elif (dict_result['modificator'] == 'interp'):
-                data = dict_result['functions'].split(';')
-                if (len(data) == 2):
+                    os.remove(approx.get_plot_name())
+                elif len(data) == 3:
                     x = data[0].split()
                     y = data[1].split()
-                    fig_name = telemath.plot_interp(x, y)
-                    plot_image = open(fig_name, 'rb')
+                    lower_b = float(data[2].split()[0])
+                    upper_b = float(data[2].split()[1])
+
+                    approx = TeleMath.Mathematica.Approximation()
+                    approx.set_xy(x, y)
+                    approx.set_bounds(lower_b, upper_b)
+                    approx.approximate()
+                    plot_image = open(approx.get_plot_name(), 'rb')
 
                     await bot.send_photo(message.chat.id, plot_image)
 
                     plot_image.close()
-                    os.remove(fig_name) 
-            elif (dict_result['modificator'] == 'integ'):
+                    os.remove(approx.get_plot_name())
+            elif dict_result['modificator'] == 'interp':
                 data = dict_result['functions'].split(';')
-                if (len(data) == 2):
+                if len(data) == 2:
+                    x = data[0].split()
+                    y = data[1].split()
+                    interp = TeleMath.Mathematica.Interpolation()
+                    interp.set_xy(x, y)
+                    interp.interpolate()
+                    plot_image = open(interp.get_plot_name(), 'rb')
+
+                    await bot.send_photo(message.chat.id, plot_image)
+
+                    plot_image.close()
+                    os.remove(interp.get_plot_name()) 
+            elif dict_result['modificator'] == 'integ':
+                data = dict_result['functions'].split(';')
+                if len(data) == 2:
                     func = data[0]
                     borders = data[1].split() 
                     a = borders[0]
@@ -392,7 +478,7 @@ async def main(message):
                     result = telemath.optimize_number(result)
 
                     await bot.send_message(message.chat.id, f'({func})dx = {result}')
-            elif (dict_result['modificator'] == 'dev'):
+            elif dict_result['modificator'] == 'dev':
                 numbers = dict_result['functions'].split()
                 numbers = [float(k) for k in numbers]
                 result = telemath.calculate_std_dev(numbers)
